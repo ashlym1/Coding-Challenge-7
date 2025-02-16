9c2842f7a5a8c954decfc19fba50c196012bc2df
ashlym1 <ashlym3@usf.edu>
2025-02-16 17:51:12 -0500

    Task 3 - Customer Loyalty Discount

--- cc_7.js
+++ cc_7.js
@@ -1,4 +1,4 @@
-// Task 1 - Function Declaration: Customer Invoice Calculation 
+// Task 1 - Function Declaration:
 function calculateInvoice(subtotal, taxRate, discount) {
     let total = (subtotal + (subtotal * taxRate)) - discount; 
     return "Total Invoice: $" + total.toFixed(2);
@@ -7,7 +7,7 @@ function calculateInvoice(subtotal, taxRate, discount) {
 console.log(calculateInvoice(100, 0.08, 5)); // Expected Output: "Total Invoice: $103.00"
 console.log(calculateInvoice(500, 0.1, 20)); // Expected Output: "Total Invoice: $530.00"
 
-// Task 2 - Function Expression; Employee Hourly wage calulation
+// Task 2 - Function Expression: 
 const calculateHourlyWage = function (salary, hoursPerWeek) { 
 // Calculating the total hrs working per yr (Instruction said 52)
  let totalHoursPaperYear = hoursPerWeek * 52;
@@ -19,3 +19,15 @@ return `Hourly Wage: $$ { hourlyWage.toFixed(2)}`;
 // Test Data
 console.log(calculateHourlyWage(52000, 40)); // Expected Output: "Hourly Wage: $25.00"
 console.log(calculateHourlyWage(75000, 35)); // Expected Output: "Hourly Wage: $41.21"
+
+// Task 3- Arrow Function : 
+const calculateLoyaltyDiscount = (amount,years)=>{ 
+    // Discount depends on the customer loyalty %
+    let discountRate= years >= 5 ? 0.15: years >= 3? 0.10 : 0.05;
+    //applying the discount to the original amount 
+    let discountedPrice=amount * (1-discountRate);
+    //returning the final price after dicount; the "tofixed" is to format to the 2nds decimal place )
+};
+//  Test data 
+console.log(calculateLoyaltyDiscount(100, 6)); // Expected Output: "Discounted Price: $85.00"
+console.log(calculateLoyaltyDiscount(200, 2)); // Expected Output: "Discounted Price: $190.00"
